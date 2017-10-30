cc2d9045e1f12335427076750f836a48a2b4f5f3
Roy Morales <roy_morales35@hotmail.com>
2017-10-30 15:00:40 -0400

    updated components for buttons and textarea

--- public/scripts/widgetManager.js
+++ public/scripts/widgetManager.js
@@ -18,24 +18,23 @@ function Widget(data){
 function Component(data){
 	var componentData = data;
 	
-	if (componentData.type == "textbox"){
-		componentData.type = "textarea";
-		var rowSize = 22.5;
-		var columnSize = 40;
-	}
 	console.log("Component data type: "+componentData.type)
-	console.log("Component hint: "+componentData.hint)
+	
 	this.getHTML = function(callback) {
 		// var componentHTML = "<div> Component "+componentData.type+" </div>";
 		//Do stuff here to build the componenet
-		var component = "<"
-		+componentData.type+" "
-		+"placeholder="+componentData.hint+" "
-		+"rows="+rowSize+" "
-		+"cols="+columnSize
-		+";>"
-		+" </"+componentData.type+">"
-		console.log("Component end: "+component)
+		if (componentData.type == "textbox"){
+			var component = buildTextbox(componentData);
+			
+		} 
+		else if (componentData.type == "numberRow"){
+			var component = buildNumberRow(componentData);
+			
+		} 
+		else if (componentData.type == "button") {
+			var component = buildButton(componentData);
+		}
+
 		//When done
 		return new Promise(resolve => {
 		    setTimeout(() => {
@@ -44,3 +43,47 @@ function Component(data){
 		});
 	}
 }
+
+//Add functions to build
+
+//Textbox
+function buildTextbox(data) {
+	var component = "<textarea "
+		+"placeholder=\""+data.hint+"\" "
+	
+		+">"
+		+data.value.trim()
+		+"</textarea>"
+
+    console.log("Component end: "+component)
+    return component;
+}
+
+
+//numberRow
+function buildNumberRow(data){
+
+	var component = "<input type="
+	+"\"number\""
+	+"placeholder=\""+data.hint+"\" "
+	+">"
+	+"</input>"
+
+    console.log("Component buildNumberRow: "+component)
+    return component;
+}
+
+//numberRow
+function buildButton(data){
+
+	var component = "<br><button type="
+	+"\"button\""
+	+">"
+	+data.value
+	+"</button><br>"
+
+    console.log("Component buildButton: "+component)
+    return component;
+}
+
+
